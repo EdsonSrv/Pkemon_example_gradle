a06dc87207ac1e7977ef11f85eacb9d3b7093b9a
Edson Srvn <edson@makingdevs.com>
2019-09-04 13:34:52 -0500

    codign #1  remove unused code

--- src/main/groovy/com/pokemon/Application.groovy
+++ src/main/groovy/com/pokemon/Application.groovy
@@ -6,11 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class Application {
 
-// Every method is public as well
 	static void main(String[] args) {
 
- 	// You can omit the last method call parenthesis
- 	// This is the same as .run(Application, args)
  		SpringApplication.run Application, args
  	}
 }
